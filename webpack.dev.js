088c865ab95d3048c7933510b5ee6fa447b2abbf
Richard Sheppard <richard@siliconmeadow.net>
2023-06-01 16:21:27 +0200

    Introduce webpack-merge to make dev/prod configs DRY.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,44 +1,23 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
-  entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, './dist')
+    static: './dist'
   },
 
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
         test: /\.scss$/i,
         use: [
-          {loader: 'css-loader', options: {sourcemap: true}},
-          {loader: 'postcss-loader', options: {sourcemap: true}},
-          {loader: 'sass-loader', options: {sourcemap: true}},
+          {loader: 'css-loader', options: {sourceMap: true}},
+          {loader: 'postcss-loader', options: {sourceMap: true}},
+          {loader: 'sass-loader', options: {sourceMap: true}},
         ]
       }
     ]
   },
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    })
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,12 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { Node } = require('postcss');
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'production',
-  entry: './src/index.js',
-  devServer: {
-    static: path.resolve(__dirname, './dist')
-  },
 
   module: {
     rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
       {
         test: /\.scss$/i,
         use: [
@@ -38,15 +23,8 @@ module.exports = {
   },
   
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
     })
   ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  }
-};
+});
