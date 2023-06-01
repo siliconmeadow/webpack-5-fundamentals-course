ab656adb4245671f436f7593ee5520546467455e
Richard Sheppard <richard@siliconmeadow.net>
2023-06-01 20:24:50 +0200

    Added jest for unit testing and a lame example test was created.

---
+++ src/__tests__/sum.test.js
@@ -0,0 +1,5 @@
+import sum from '../js/sum'
+
+test('adds 2 numbers together', () => {
+  expect(sum(1,2)).toBe(3)
+});
--- src/js/sum.js
+++ src/js/sum.js
@@ -1,3 +1,9 @@
 import _ from 'lodash';
 
 console.log(_.join(['Sum', 'module', 'loaded!'], ' '));
+
+function sum(a, b) {
+  return a + b
+}
+
+export default sum
